697cd4c083dba5141189c83e389cc4105233782d
Antonio Torrini <antorrin@silabs.com>
2020-10-02 09:58:56 -0500

    added BatchNormalization to improve training's convergence

--- eembc/TFLite_micro_speech/model.py
+++ eembc/TFLite_micro_speech/model.py
@@ -2,25 +2,26 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Dropout, BatchNormalization, Activation
 
 def micro_speech_eembc():
-	model = Sequential()
-	model.add(Input(
-		[49, 40 ,1]))
-	model.add(Conv2D(
-		filters=8,
-		kernel_size=(10, 8),
-		use_bias=True,
-		padding="same",
-		activation="relu",
-		strides=(2,2)))
-	model.add(Dropout(
-		rate=0.5))
-	model.add(Flatten())
-	model.add(Dense(
-		units=4,
-		use_bias=True,
-		activation='softmax'))
-	return model
+  model = Sequential()
+  model.add(Input(
+	  [49, 40 ,1]))
+  model.add(Conv2D(
+	  filters=8,
+	  kernel_size=(10, 8),
+	  use_bias=True,
+	  padding="same",
+	  strides=(2,2)))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(Dropout(
+	  rate=0.5))
+  model.add(Flatten())
+  model.add(Dense(
+	  units=4,
+	  use_bias=True,
+	  activation='softmax'))
+  return model
 
