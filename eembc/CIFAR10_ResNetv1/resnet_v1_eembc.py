4f637a81d82305b153a08b9956fb0568890021ad
Javier Duarte <jduarte@ucsd.edu>
2021-02-03 15:15:32 -0600

    add tiny model

--- eembc/CIFAR10_ResNetv1/resnet_v1_eembc.py
+++ eembc/CIFAR10_ResNetv1/resnet_v1_eembc.py
@@ -7,8 +7,8 @@ from tensorflow.keras.layers import Conv2D, AveragePooling2D, MaxPooling2D, Add
 from tensorflow.keras.regularizers import l1_l2
 
 #define model
-def resnet_v1_eembc(input_shape=[32,32,3], num_classes=10, num_filters=[16,32,64], 
-                    kernel_sizes=[3,1], strides=[1,2], l1p=1e-4, l2p=0):
+def resnet_v1_eembc(input_shape=[32, 32, 3], num_classes=10, num_filters=[16, 32, 64], 
+                    kernel_sizes=[3, 1], strides=[1, 2], l1p=1e-4, l2p=0):
 
     # Input layer, change kernel size to 7x7 and strides to 2 for an official resnet
     inputs = Input(shape=input_shape)
@@ -101,6 +101,63 @@ def resnet_v1_eembc(input_shape=[32,32,3], num_classes=10, num_filters=[16,32,64
 
     # Overall residual, connect weight layer and identity paths
     x = Add()([x, y])
+    x = Activation('relu')(x)
+
+    # Final classification layer.
+    pool_size = int(np.amin(x.shape[1:3]))
+    x = AveragePooling2D(pool_size=pool_size)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def resnet_v1_eembc_tiny(input_shape=[32, 32, 3], num_classes=10, num_filters=[8], 
+                         kernel_sizes=[3, 1], strides=[1, 2], l1p=1e-4, l2p=0):
+
+    # Input layer, change kernel size to 7x7 and strides to 2 for an official resnet
+    inputs = Input(shape=input_shape)
+    x = Conv2D(num_filters[0],
+                  kernel_size=kernel_sizes[0],
+                  strides=strides[0],
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l1_l2(l1=l1p,l2=l2p))(inputs)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # First stack
+    # Weight layers
+    y = Conv2D(num_filters[0],
+                  kernel_size=kernel_sizes[0],
+                  strides=strides[1],
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l1_l2(l1=l1p,l2=l2p))(x)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+    y = Conv2D(num_filters[0],
+                  kernel_size=kernel_sizes[0],
+                  strides=strides[0],
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l1_l2(l1=l1p,l2=l2p))(y)
+    y = BatchNormalization()(y)
+
+    # Adjust for change in dimension due to stride in identity
+    x = Conv2D(num_filters[0],
+                  kernel_size=kernel_sizes[1],
+                  strides=strides[1],
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l1_l2(l1=l1p,l2=l2p))(x)
+
+    # Overall residual, connect weight layer and identity paths
+    x = Add()([x, y]) 
     x = Activation('relu')(x)
 
     # Final classification layer.
