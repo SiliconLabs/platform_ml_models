5dae93f543a9d54e60797621e04d3fd057d0693f
Nat Jeffries <natmjeffries@gmail.com>
2021-06-28 17:21:49 -0700

    Add script to generate y_labels

---
+++ eembc/Person_detection/create_ground_truth.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+
+GROUND_TRUTH_FILENAME='y_labels.csv'
+PERSON_DIR = 'vw_coco2014_96_2p5b_test/person'
+NON_PERSON_DIR = 'vw_coco2014_96_2p5b_test/non_person'
+
+def generate_file_contents():
+  outfile = open(GROUND_TRUTH_FILENAME, "a") 
+
+  for file in os.listdir(PERSON_DIR):
+    input_filename = os.fsdecode(file)
+    line = '%s, %d, %d\n'%(input_filename, 2, 1)
+    outfile.write(line)
+  
+  for file in os.listdir(NON_PERSON_DIR):
+    input_filename = os.fsdecode(file)
+    line = '%s, %d, %d\n'%(input_filename, 2, 0)
+    outfile.write(line)
+
+  outfile.flush()
+  outfile.close()
+
+generate_file_contents()
